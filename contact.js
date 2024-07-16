48887374c70944511eb6d8131537100184321b54
ostap4308 <o42096372@gmail.com>
2024-07-16 20:46:07 +0300

    Removed building photos, organized content into folders similar to News and Objects, extracted all news items from both folders, gathered job vacancies, added top and bottom blocks to all pages, updated partner photos, and created styles for each object and news item.

--- contact.js
+++ contact.js
@@ -1,14 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var contactsBtn = document.getElementById("contactsBtn"); // Отримуємо кнопку "Контакти"
-    var catalogPopup = document.getElementById("catalogPopup"); // Отримуємо вікно каталогу
+// Додаємо обробник події для натискання на кнопку "Контакти"
+contactsBtn.addEventListener("click", function() {
+    // Перевіряємо, чи вікно каталогу видиме, якщо так, то приховуємо його, якщо ні - показуємо
+    if (catalogPopup.style.display === "block") {
+        catalogPopup.style.display = "none"; // Приховуємо вікно каталогу
+        catalogPopupBackground.style.display = "none"; // Приховуємо фоновий шар
+    } else {
+        catalogPopup.style.display = "block"; // Показуємо вікно каталогу
+        catalogPopupBackground.style.display = "block"; // Показуємо фоновий шар
+    }
+});
 
-    // Додаємо обробник події для натискання на кнопку "Контакти"
-    contactsBtn.addEventListener("click", function() {
-        // Перевіряємо, чи вікно каталогу видиме, якщо так, то приховуємо його, якщо ні - показуємо
-        if (catalogPopup.style.display === "block") {
-            catalogPopup.style.display = "none"; // Приховуємо вікно каталогу
-        } else {
-            catalogPopup.style.display = "block"; // Показуємо вікно каталогу
-        }
-    });
+// Додаємо обробник події для натискання на саме вікно каталогу
+catalogPopup.addEventListener("click", function() {
+    catalogPopup.style.display = "none"; // Приховуємо вікно каталогу
+    catalogPopupBackground.style.display = "none"; // Приховуємо фоновий шар
+});
+
+// Додаємо обробник події для натискання на фоновий шар
+catalogPopupBackground.addEventListener("click", function() {
+    catalogPopup.style.display = "none"; // Приховуємо вікно каталогу
+    catalogPopupBackground.style.display = "none"; // Приховуємо фоновий шар
 });
